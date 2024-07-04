f1c5eb0013b78887f8c0812cc83e63f464e75cf8
Boris Kaminskiy <93283934+BorisKaminskiy@users.noreply.github.com>
2024-07-04 12:49:26 +0300

    Update Layout.tsx

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,12 +3,16 @@ import Header from "./components/Header/Header";
 import { Outlet } from "react-router";
 import cn from 'classnames'
 import styles from './Layout.module.scss'
+import backgroundImage from '../assets/img/cart2-CGtZG1e2-min.jpg'
 
 const Layout: FC = () => {
   return (
     <div className={cn(styles.layout)}>
       <Header />
-      <div className={cn(styles.map_container)}>
+      <div className={ cn(styles.map_container) }>
+        <div className={ cn(styles.image) }>
+          <img style={{width: '100%', height: 'фгещ'}} src={ backgroundImage } alt="map" />
+        </div>
         <Outlet />
       </div>
     </div>
